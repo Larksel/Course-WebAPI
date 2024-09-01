fff912457c46fb580cea6328d5d6acd966ce6f49
Larksel <lemuelgames@outlook.com>
2024-09-01 18:57:51 -0300

    :sparkles: feat: LayoutPage criada

---
+++ frontend/src/components/Layout/LayoutPage.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+
+const LayoutPage = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const token = Cookies.get('login');
+
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  if (!isLoggedIn) {
+    navigate('/');
+  }
+
+  return (
+    <div className='flex flex-col min-h-screen overflow-hidden'>
+      <div className='flex fixed w-full top-0 z-10' style={{ minHeight: '50px'}}>
+        {/* Aqui ficará o navbar */}
+        <Navbar />
+      </div>
+      <div className='flex flex-row flex-grow mt-[56px] sm:mt-[64px]'>
+        {/* Sidebar fixa na lateral esquerda */}
+        <div className='flex fixed h-full top-[56px] sm:top-[64px] z-10'>
+          <Sidebar />
+        </div>
+
+        {/* Conteúdo principal adaptável */}
+        <div
+          className='flex-grow overflow-y-auto scrollable-container'
+          style={{
+            paddingLeft: '265px',
+            paddingTop: '15px',
+            paddingRight: '15px',
+            paddingBottom: '15px',
+            maxHeight: 'calc(1000vh - 64px)',
+          }}
+        >
+          <div className=''>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LayoutPage;
