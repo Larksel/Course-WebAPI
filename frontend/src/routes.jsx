3667256897d683f525891bb7bce039aff3a51cc3
Larksel <lemuelgames@outlook.com>
2024-09-01 18:58:05 -0300

    :sparkles: feat: Home criada

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/login';
+import Home from './pages/home';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />}/>
+        <Route path='/' element={<Login />}/> 
+        <Route path='/inicio' element={<Home />}/> 
       </Routes>
     </Router>
   )
